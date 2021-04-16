ea02b7412fe84342f7de0a45f7dba1f5a1cbca36
David Cox-Espenlaub <daveespionage@gmail.com>
2021-04-16 16:17:55 -0700

    fix: error handler

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,22 +1,36 @@
+import React from 'react'
 import NextErrorComponent from 'next/error'
 import * as Sentry from '@sentry/node'
 
-const MyError = ({ statusCode, hasGetInitialPropsRun, err }) => {
+const MyError = ({
+  statusCode,
+  hasGetInitialPropsRun,
+  err,
+}: any): React.ReactElement => {
   if (!hasGetInitialPropsRun && err) {
     // getInitialProps is not called in case of
     // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
     // err via _app.js so it can be captured
     Sentry.captureException(err)
-    // Flushing is not required in this case as it only happens on the client
   }
 
   return <NextErrorComponent statusCode={statusCode} />
 }
 
-MyError.getInitialProps = async ({ res, err, asPath }) => {
-  const errorInitialProps = await NextErrorComponent.getInitialProps({
+MyError.getInitialProps = async ({
+  res,
+  err,
+  asPath,
+  pathname,
+  query,
+  AppTree,
+}): Promise<any> => {
+  const errorInitialProps: any = await NextErrorComponent.getInitialProps({
     res,
     err,
+    pathname,
+    query,
+    AppTree,
   })
 
   // Workaround for https://github.com/vercel/next.js/issues/8592, mark when
@@ -36,13 +50,13 @@ MyError.getInitialProps = async ({ res, err, asPath }) => {
   //    Boundary. Read more about what types of exceptions are caught by Error
   //    Boundaries: https://reactjs.org/docs/error-boundaries.html
 
+  if (res?.statusCode === 404) {
+    // Opinionated: do not record an exception in Sentry for 404
+    return { statusCode: 404 }
+  }
   if (err) {
     Sentry.captureException(err)
-
-    // Flushing before returning is necessary if deploying to Vercel, see
-    // https://vercel.com/docs/platform/limits#streaming-responses
     await Sentry.flush(2000)
-
     return errorInitialProps
   }
 
