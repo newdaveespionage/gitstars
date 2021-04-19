511caf868d97bfc0123c2a90d2a57d51e42ceebc
David Cox-Espenlaub <daveespionage@gmail.com>
2021-04-18 19:42:57 -0700

    feat: add blues

--- theme/index.ts
+++ theme/index.ts
@@ -1,4 +1,9 @@
 export const colors = {
   default: '#000000',
   border: '#eaeaea',
+  blue100: '#000055',
+  blue300: '#000055',
+  blue500: '#000088',
+  blue800: '#0000DD',
+  white: '#ffffff',
 }
