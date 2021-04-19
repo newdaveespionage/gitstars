2a5feb7fcaf30d07f23bb32513f93117b4ebeb90
David Cox-Espenlaub <daveespionage@gmail.com>
2021-04-18 19:46:49 -0700

    refactor: blues

--- theme/index.ts
+++ theme/index.ts
@@ -2,8 +2,8 @@ export const colors = {
   default: '#000000',
   border: '#eaeaea',
   blue100: '#000055',
-  blue300: '#000055',
-  blue500: '#000088',
-  blue800: '#0000DD',
+  blue300: '#111155',
+  blue500: '#2222DD',
+  blue600: '#6666ff',
   white: '#ffffff',
 }
