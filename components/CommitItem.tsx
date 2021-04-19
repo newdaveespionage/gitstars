49e08704dd1d63b2c1e0f8f3874bde1478dd6565
David Cox-Espenlaub <daveespionage@gmail.com>
2021-04-18 20:29:09 -0700

    refactor: make commit item fancy

--- components/CommitItem.tsx
+++ components/CommitItem.tsx
@@ -14,9 +14,30 @@ const CommitItem = ({
   message,
 }: CommitItemProps): JSX.Element => (
   <div key={id} className="card">
-    <div>{name}</div>
-    <div>{DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_SHORT)}</div>
-    <div>{message}</div>
+    <div className="commit-header">
+      <span className="name">{name}</span>
+      <span className="date">
+        {DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_SHORT)}
+      </span>
+    </div>
+    <div className="message">{message}</div>
+    <style jsx>{`
+      .commit-header {
+        display: flex;
+        justify-content: space-between;
+      }
+      .name,
+      .date,
+      .message {
+        line-height: 1.5rem;
+      }
+      .name {
+        font-weight: bold;
+      }
+      .message {
+        font-size: 0.9rem;
+      }
+    `}</style>
   </div>
 )
 
