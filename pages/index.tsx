a85bac54976c785569928d83acea79f202157f11
David Cox-Espenlaub <daveespionage@gmail.com>
2021-04-19 10:09:34 -0700

    feat: seo tags, viewport, and on-page description

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,25 @@ import Head from 'next/head'
 import Footer from '../components/Footer'
 import Repositories from '../components/Repositories'
 
+const description =
+  'See the current top 100 starred repositories on Github.com with commits from the past 24 hours'
+
 export const Home = (): JSX.Element => {
   return (
     <div className="container">
       <Head>
         <title>Gitstars</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content={`${description}. Leverages the Github REST apis, next.js, and Vercel`}
+        />
       </Head>
 
       <main>
         <h1>Gitstars</h1>
-
+        <p>{description}</p>
         <Repositories />
       </main>
       <Footer />
